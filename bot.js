054d6bc2456ad313b3ea60f1014e31d19648e9dc
opclan1 <43325079+opclan1@users.noreply.github.com>
2018-09-19 00:18:29 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -229,14 +229,14 @@ client.on('message', message => {
     } else
     
     if (message.content.startsWith(prefix + 'setname')) {
-    if (message.author.id !== '323160008411971585') return message.reply('** هذا الأمر فقط لصاحب البوت و شكراًً **')
+    if (message.author.id !== '322820141089947659') return message.reply('** هذا الأمر فقط لصاحب البوت و شكراًً **')
       client.user.setUsername(argresult).then
           message.channel.sendMessage(`**${argresult}** : تم تغير الأسم`)
       return message.reply("**لا تستطيع تغير الأسم الا بعد ساعتين**");
     } else
         
     if (message.content.startsWith(prefix + 'setavatar')) {
-    if (message.author.id !== '323160008411971585') return message.reply('** هذا الأمر فقط لصاحب البوت و شكراًً **')
+    if (message.author.id !== '322820141089947659') return message.reply('** هذا الأمر فقط لصاحب البوت و شكراًً **')
     client.user.setAvatar(argresult);
         message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
     } else
